c3a90663ee88c04c4811472c4ba3649f54abde81
Marc Wilmot <wilmot.marc@yahoo.fr>
2023-11-12 21:30:45 +0100

    Test d'un nouveau script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Utilisez le chemin du répertoire de travail GitHub Actions
-REPO_PATH=$GITHUB_WORKSPACE
-
-FILES=$(git -C "$REPO_PATH" diff --name-only --diff-filter=ACM | grep -E '\.jsx?$' || true)
+FILES=$(git diff --name-only --diff-filter=AM $((git merge-base HEAD $GITHUB_BASE_REF || echo -n $GITHUB_BASE_REF) || echo -n HEAD))
 
 if [[ -n "$FILES" ]]; then
-  echo "Running pre-commit check on changed files:"
+  echo "Running pre-commit check on staged files:"
   echo "$FILES"
   echo ""
 
   for FILE in $FILES; do
-    if grep -q "secret" "$REPO_PATH/$FILE"; then
+    if grep -q "secret" "$FILE"; then
       echo "Error: Found 'secret' keyword in $FILE. Please remove it before committing."
       exit 1
     fi
@@ -19,3 +16,4 @@ if [[ -n "$FILES" ]]; then
 fi
 
 exit 0
+
